20acd05ad985607cdef87f6c5da3dcd6b2ca444c
Alexis <alexis.reymann@telecom-paristech.fr>
2018-01-26 18:18:08 +0100

    parse refaite en entier

--- heuristique.cpp
+++ heuristique.cpp
@@ -15,69 +15,43 @@ using namespace std;
 
 int c[81][1600], a[81][1600], b[81], n, m, br[81], machine[1600], cost = 0;
 
-void parse(){
-  string s;
-  int iter;
-  char ch;
-
-  //parse n
-  getline(cin, s);
-  s.pop_back();
-  iter = s.length()-1;
-  while(s[iter] != '=' && s[iter] != ' ') iter--;
-  n = stoi(s.substr(iter+1, s.length()-iter));
-
-  //parse m
-  getline(cin, s);
-  s.pop_back();
-  iter = s.length()-1;
-  while(s[iter] != '=' && s[iter] != ' ') iter--;
-  m = stoi(s.substr(iter+1, s.length()-iter));
-
-  //parse c
-  getline(cin, s);
-  for(int j = 0; j < m; j++){
-    scanf("%c", &ch);
-    while(ch == ' ' || ch == '\n') scanf("%c", &ch);
-    for(int i = 0; i < n; i++){
-      scanf("%d", &c[j][i]);
-    }
-    if(j != m-1) scanf("%c%c", &ch, &ch);
-  }
-  scanf("%c", &ch);
-  while(ch == ' ') scanf("%c", &ch);
-  scanf("%c", &ch);
-  getline(cin, s);
-  for (int i = 0; i < n; i++) c[m][i] = 100000;
-
-  //parse a
-  getline(cin, s);
-  for(int j = 0; j < m; j++){
-    scanf("%c", &ch);
-    while(ch == ' ' || ch == '\n') scanf("%c", &ch);
-    for(int i = 0; i < n; i++){
-      scanf("%d", &a[j][i]);
-    }
-    if(j != m-1) scanf("%c%c", &ch, &ch);
-  }
-  scanf("%c", &ch);
-  while(ch == ' ') scanf("%c", &ch);
-  scanf("%c", &ch);
-  getline(cin, s);
-  for(int i = 0; i < n; i++) a[m][i] = 1;
-
-  //parse b
-  scanf("%c%c", &ch, &ch);
-  while(ch == ' ' || ch == '\n') scanf("%c", &ch);
-  scanf("%c", &ch);
-  while(ch == ' ' || ch == '\n') scanf("%c", &ch);
-  for(int j = 0; j < m; j++) scanf("%d", &b[j]);
-  b[m] = INT_MAX;
-  for(int j = 0; j <= m; j++) br[j] = b[j];
+bool isnumber(char ch){
+  return ( (ch-'0' >= 0 && ch-'0' <= 9) || ch == ' ' );
 }
 
+void parse(){
+  stringstream ss;
+  char ch;
+  while(scanf("%c", &ch) != EOF){
+    if(isnumber(ch)) ss << ch;
+    else ss << ' ';
+  }
+
+  //parse n & m
+  ss >> n >> m;
+
+  //parse c
+  for(int j = 0; j < m; j++)
+    for(int i = 0; i < n; i++) ss >> c[j][i];
+
+  //parse a
+  for(int j = 0; j < m; j++)
+    for(int i = 0; i < n; i++) ss >> a[j][i];
+
+  //parse b
+  for(int j = 0; j < m; j++) ss >> b[j];
+  b[m] = INT_MAX;
+
+  for(int j = 0; j <= m; j++) br[j] = b[j];
+
+  for(int i = 0; i < m; i++) printf("%d ", b[i]);
+}
+
+
 int main(){
-  parse();
+
+  parse2();
+  
   for(int i = 0; i < n; i++){
     int mina = 100000;
     int minIndex = m;
